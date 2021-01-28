0bb7fe1fbe87c8844adb12121289156f3dba2c53
Timur <timuriagudin@yahoo.com>
2021-01-28 15:47:31 -0500

    config selenoid

---
+++ selenoid.conf.js
@@ -0,0 +1,28 @@
+const hooks = require('./config/hooks.conf');
+const base = require('./config/base.conf');
+
+exports.config = {
+    hostname: '86.107.197.188',
+    port: 4444,
+    path: '/wd/hub',
+    maxInstances: 1,
+    capabilities: [
+        {
+            browserName: 'chrome',
+            'selenoid:options': {
+                enableVNC: true,
+                enableVideo: true,
+            },
+        },
+        {
+            browserName: 'firefox',
+            'selenoid:options': {
+                enableVNC: true,
+                enableVideo: true,
+            },
+        }
+    ],
+    baseUrl: 'https://stage.localcoding.us',
+    ...base,
+    ...hooks
+}
